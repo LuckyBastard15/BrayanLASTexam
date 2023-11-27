afb094d8a21b86bfc15746c638dc8bbf41736bb5
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2023-11-26 19:20:55 -0600

    firstcommit

---
+++ Source/BrayanLastExam/BrayanLastExam.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BrayanLastExam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrayanLastExam, "BrayanLastExam" );
+ 
